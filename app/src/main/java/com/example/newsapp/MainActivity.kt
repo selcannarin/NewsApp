cf75a40f304925915802b9ff8e5b62e1a622cf9a
selcannarin <narinselcan@gmail.com>
2023-10-21 14:51:59 +0300

    set bottom nav menu and check network connection

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.example.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.example.newsapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        if (!isNetworkConnected()) {
+            showNoInternetDialog()
+        } else {
+            setContentView(binding.root)
+            setupNavigation()
+        }
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
+    }
+
+    private fun showNoInternetDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("No Internet Connection")
+        alertDialogBuilder.setMessage("Please check your internet connection.")
+        alertDialogBuilder.setNegativeButton("CLOSE APPLICATION") { _, _ ->
+            finish()
+        }
+        alertDialogBuilder.setCancelable(false)
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.news -> {
+                    navController.navigate(R.id.newsFragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.favorites -> {
+                    navController.navigate(R.id.favoritesFragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> {
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
+    }
+
+    fun setToolbarTitle(title: String) {
+        supportActionBar?.title = title
     }
 }
