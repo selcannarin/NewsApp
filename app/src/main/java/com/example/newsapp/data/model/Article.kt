54456cb4efe13a08e73bac8ada04458d5ed7f922
selcannarin <narinselcan@gmail.com>
2023-10-21 15:48:26 +0300

    create models

---
+++ app/src/main/java/com/example/newsapp/data/model/Article.kt
@@ -0,0 +1,15 @@
+package com.example.newsapp.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Article(
+    @SerializedName("author") val author: String,
+    @SerializedName("content") val content: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("publishedAt") val publishedAt: String,
+    @SerializedName("source") val source: Source,
+    @SerializedName("title") val title: String,
+    @SerializedName("url") val url: String,
+    @SerializedName("urlToImage") val urlToImage: String
+)
