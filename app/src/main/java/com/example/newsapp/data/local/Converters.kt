0d9e6782216a0192e5ab88f9bde87100eb308115
selcannarin <narinselcan@gmail.com>
2023-10-21 17:44:21 +0300

    implement room

---
+++ app/src/main/java/com/example/newsapp/data/local/Converters.kt
@@ -0,0 +1,17 @@
+package com.example.newsapp.data.local
+
+import androidx.room.TypeConverter
+import com.example.newsapp.data.model.Source
+
+class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/data/local/NewsDao.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.newsapp.data.model.Article
+
+@Dao
+interface NewsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
